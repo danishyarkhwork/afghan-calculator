9c3a90546e4f2f099e99373bd1f5fb743c5b9ada
danishyarkhwork <danishyarkhwork@gmail.com>
2024-06-28 20:22:35 +0430

    fix: logo update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -64,7 +64,7 @@ const Header = () => {
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/calc-logo.png"
+                  src="/images/logo/afg-calc-white.png"
                   alt="logo"
                   width={140}
                   height={30}
