116fc4847773a34d784e93b6b743146f64799740
danishyarkhwork <danishyarkhwork@gmail.com>
2024-06-29 22:53:03 +0430

    fix: home page tool added

--- src/components/OurToolsSection/ourToolsSectionData.tsx
+++ src/components/OurToolsSection/ourToolsSectionData.tsx
@@ -4,7 +4,7 @@ const ourToolsSectionData = [
   { id: 3, emoji: "😊", title: "Afghan AI Emoji Generator", description: "Create & Find Emoji with Free AI", link: "/afghan-emoji-generator" },
   { id: 4, emoji: "🛍️", title: "Sales Tax Calculator", description: "Sales tax of goods and services.", link: "/sales-tax-calculator" },
   { id: 5, emoji: "🙋‍♂️", title: "Age Calculator", description: "Calculate your age here.", link: "/age-calculator" },
-  { id: 6, emoji: "💻", title: "Tool Six", description: "Description for tool six.", link: "#" },
+  { id: 6, emoji: "💻", title: "Free Typing Tool", description: "Practice your typing speed here.", link: "/afghan-typing-tool" },
   { id: 7, emoji: "🔒", title: "Tool Seven", description: "Description for tool seven.", link: "#" },
   { id: 8, emoji: "🧩", title: "Tool Eight", description: "Description for tool eight.", link: "#" },
   // Add more tools as needed
